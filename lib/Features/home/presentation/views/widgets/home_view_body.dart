71752520aa9a0401a6379b0153b59a0012a8aac9
mellatilaid <laidmellati@gmail.com>
2024-06-30 12:06:40 +0200

    created text styled class & added text to home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,9 +9,18 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         FeaturedListView(),
+        SizedBox(height: 50),
+        Padding(
+          padding: EdgeInsets.only(left: 16.0),
+          child: Text(
+            'Best Sellers',
+            style: Styles.titleMeduim,
+          ),
+        ),
       ],
     );
   }
